23e325feb2854b5da88b8cd03ada8f63128189d8
speyyeti068 <75049648+speyyeti068@users.noreply.github.com>
2021-04-16 18:42:31 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 function setup() {
   canvas = createCanvas(300, 300);
   canvas.center();
+
   video = createCapture(VIDEO);
   video.hide();
+
+  classifier = ml5.imageClassifier("MobileNet",()=>{
+    console.log("Model Loaded");
+  });
 }
 
+function draw(){
+  image(video, 0,0,300,300);
 
+  classifier.classify(video,(error,result)=>{
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(result);
+
+      document.getElementById("object").innerHTML = result[0].label;
+      document.getElementById("accuracy").innerHTML = (result[0].confidence * 100).toFixed(2) + "%";
+
+    }
+  })
+}
 
